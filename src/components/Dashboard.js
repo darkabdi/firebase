1f736fd7cea94b5fdcf65a6135c52fa00300b3d6
abdi mohamed <aabdymohamed@gmail.com>
2023-03-03 13:35:01 +0100

    små ändringar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import { Card, Button, Alert } from "react-bootstrap"
 import { useAuth } from "../context/AuthContext"
-import { Link, useHistory } from "react-router-dom"
+import { Link,} from "react-router-dom"
 
 export default function Dashboard() {
   const [error, setError] = useState("")
